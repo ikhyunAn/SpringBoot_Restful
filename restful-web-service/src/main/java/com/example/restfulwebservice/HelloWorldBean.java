63f507424c0700210f7f5583b3e8a56ee1279b5d
ikhyunAn <johnan0077@gmail.com>
2023-01-18 23:09:11 -0500

    un-git then add

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// lombok
+@Data //lombok data is added so that properties of HelloWorldBean are given setter / getter / constructors
+@AllArgsConstructor //constructor method is given
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+    //getters and setters and constructors not needed thanks to lombok Data
+}
