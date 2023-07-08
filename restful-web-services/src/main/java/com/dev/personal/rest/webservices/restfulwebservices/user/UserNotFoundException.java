5a43d4200202c51a4d6104cf83b29d9fc5ff3c7b
ikhyunan <johnan0077@gmail.com>
2023-07-08 18:56:06 +0900

    usernotfound exception

---
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserNotFoundException.java
@@ -0,0 +1,6 @@
+package com.dev.personal.rest.webservices.restfulwebservices.user;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String s) {
+    }
+}
