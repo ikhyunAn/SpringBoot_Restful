d5ed4e1f1df90076cfd956214f745a3fb70df419
ikhyunan <johnan0077@gmail.com>
2023-07-08 18:09:09 +0900

    create a new user

--- restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -12,14 +12,27 @@ public class UserDaoService {
     // use JPA/ Hibernate to talk to DB
     // UserDaoService > Static List
     private static List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1, "Person1", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Person2", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Person3", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Person1", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Person2", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Person3", LocalDate.now().minusYears(20)));
 
     }
     public List<User> findAll() {
         return users;
+    }
+
+    /**
+     *
+     * @param user
+     * @return user
+     */
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
     public User findOne(int id) {
         /*
--- restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,6 @@
 package com.dev.personal.rest.webservices.restfulwebservices.user;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @RestController
 public class UserResource {
@@ -9,6 +8,8 @@ public class UserResource {
     public UserResource(UserDaoService service) {
         this.service = service;
     }
+
+    // Get /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
@@ -16,5 +17,20 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        /*
+        @RequestBody annotation indicates that the method should be bound to the body of the web request.
+        The body of the request is passed through an HttpMessageConverter
+
+        Tips:
+        use Chrome extension: "Talend API" to test POST / GET APIs
+
+        Expected output:
+        200 (stands for success)
+         */
+        service.save(user);
     }
 }
