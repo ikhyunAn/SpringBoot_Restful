5a19e14d957d9d63029238bc99c36a35fb152a57
ikhyunan <johnan0077@gmail.com>
2023-07-08 17:49:27 +0900

    findOne method

--- restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,8 +1,13 @@
 package com.dev.personal.rest.webservices.restfulwebservices.user;
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+@Component
 public class UserDaoService {
     // use JPA/ Hibernate to talk to DB
     // UserDaoService > Static List
@@ -15,5 +20,14 @@ public class UserDaoService {
     }
     public List<User> findAll() {
         return users;
+    }
+    public User findOne(int id) {
+        /*
+        take in a list
+        convert to stream
+        filtering based on a predicate (which checks if the id matches)
+         */
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
