919ff99d9d2711d320516fe90d6dd1602a0ae58f
ikhyunan <johnan0077@gmail.com>
2023-07-11 22:16:12 +0900

    added openapi dependency links

--- restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
-
+	/*
+	Check: http://localhost:8080/swagger-ui/index.html
+	for swagger openapi
+	Check: http://localhost:8080/v3/api-docs
+	for vs openapi
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
