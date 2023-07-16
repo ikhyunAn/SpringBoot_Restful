d3a26e6ea5ec7779c5ae46ea98ddc2925a1f39a7
ikhyunan <johnan0077@gmail.com>
2023-07-16 14:21:35 +0900

    Internalization and Versioning

--- restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/dev/personal/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.dev.personal.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //REST API
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -16,5 +24,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}") // using GetMapping can replace "method = "
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//        return "Hello World";
     }
 }
